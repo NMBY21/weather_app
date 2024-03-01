c0b425d1ebb67e1a5bc9da8947f5c3a8fafd485f
NMBY21 <betelihemmengist21@gmail.com>
2024-03-01 15:52:02 +0300

    js form

---
+++ 2_SC_plus/W_4/challenge_5.js
@@ -0,0 +1,13 @@
+let liss = document.querySelector("li");
+let day = document.querySelector(".day");
+let paragraph = document.querySelector("ul#list p");
+
+let lis = document.querySelectorAll("li");
+let paragraphs = document.querySelectorAll("li#special p");
+
+let lisss = document.querySelector("li#special");
+li.classList.remove("liked");
+li.classList.add("something");
+
+let li = document.querySelector("li");
+li.innerHTML = "Hello World";
